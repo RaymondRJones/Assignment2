65ba3fd4da93ceae1198bbaa143ab82db755cf76
Raymond jones <Rayjones2170@gmail.com>
2018-09-06 15:01:46 -0400

    Lists all items in the correct segment

--- listingController.js
+++ listingController.js
@@ -7,8 +7,17 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      //Needs Code, Name, coordinates, and address as user input
+        //How to create an object given those variables?
+    };
+    $scope.deleteListing = function(index) {
+      listings.splice(index,1);
+    };
+    $scope.showDetails = function(index) {
+      $scope.coordinateX = listings[index].coordinates[0];
+      $scope.coordinateY = listings[index].coordinates[1];
+      $scope.address = listings[index].address;
+    };
   }
 ]);
